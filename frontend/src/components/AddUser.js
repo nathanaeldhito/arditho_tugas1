48bfa63be42d36aa5610095692c54d6631b7c1a6
nathanaeldhito <nathanaeldhito@gmail.com>
2025-03-05 18:16:04 +0700

    first commit

---
+++ frontend/src/components/AddUser.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const AddUser = () => {
+  const [name, setName] = useState("");
+  const [judul, setJudul] = useState("");
+  const [catatan, setCatatan] = useState("Male");
+  const navigate = useNavigate();
+
+  const saveUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/users", {
+        name,
+        judul,
+        catatan,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="columns mt-5 is-centered">
+      <div className="column is-half">
+        <form onSubmit={saveUser}>
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Judul</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={judul}
+                onChange={(e) => setJudul(e.target.value)}
+                placeholder="Judul"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Catatan</label>
+            <div className="control">
+              <div className="select is-fullwidth">
+                <select
+                  value={catatan}
+                  onChange={(e) => setCatatan(e.target.value)}
+                >
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="field">
+            <button type="submit" className="button is-success">
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
