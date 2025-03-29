03b1066ccecd56529057341267ce6365fc138fd9
nathanaeldhito <162309152+nathanaeldhito@users.noreply.github.com>
2025-03-29 23:00:10 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('notescrud', 'root', '',{
-host: 'localhost',
+const db = new Sequelize('tcc-2', 'root', 'dito12#$',{
+host: '34.172.81.129',
 dialect: 'mysql'
 });
 
